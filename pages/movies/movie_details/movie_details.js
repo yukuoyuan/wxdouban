b87400943d5d59aee3aed3ec5b6e7f22d0511c19
于阔源 <yukuoyuan@hotmail.com>
2018-04-13 19:22:47 +0800

    提交了电影详情界面的实现

--- pages/movies/movie_details/movie_details.js
+++ pages/movies/movie_details/movie_details.js
@@ -1,13 +1,14 @@
 // pages/movies/movie_details/movie_details.js
 const httpUtils = require('../../../utils/netutils.js');
 const movieDetailsUrls = require('../../../urlconfig.js').movieDetailsUrl;
+const utils = require('../../../utils/util.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    movieDetails: {}
   },
 
   /**
@@ -21,12 +22,47 @@ Page({
    * 获取电影详情数据
    */
   getMovieDetails: function (id) {
-    var data={};
-    httpUtils.http(movieDetailsUrls+"/"+id,data,this.successCallBack);
+    var data = {};
+    httpUtils.http(movieDetailsUrls + "/" + id, data, this.successCallBack);
   },
   /**
    *成功之后的回调
    */
-  successCallBack:function(res){
+  successCallBack: function (res) {
+    /**
+     * 处理导演信息
+     */
+    var director = {
+      name: '',
+      id: '',
+      avatar: ''
+    }
+    if (res.data.directors[0] != null) {
+      director.name = res.data.directors[0].name;
+      director.id = res.data.directors[0].id;
+      director.avatar = res.data.directors[0].avatars.large;
+    }
+    /**
+     * 处理其他电影信息
+     */
+    var movieDetails = {
+      movieImg: res.data.images ? res.data.images.large : "",
+      country: res.data.countries[0],
+      title: res.data.title,
+      originalTitle: res.data.original_title,
+      wishCount: res.data.wish_count,
+      commentCount: res.data.comments_count,
+      years: res.data.year,
+      genres: res.data.genres.join('/'),
+      stars: utils.starscanvas2Array(res.data.rating.stars),
+      score: res.data.rating.averge,
+      director: director,
+      summary: res.data.summary,
+      casts: utils.converToCastString(res.data.casts),
+      castsInfo: utils.converToCastInfo(res.data.casts)
+    }
+    this.setData({
+      movieDetails: movieDetails
+    });
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -15,7 +15,42 @@ function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
-
+/**
+ * 星星转换为一个数组
+ */
+function starscanvas2Array(stars) {
+  var num = stars.substring(0, 1);
+  var array = [];
+  for (var i = 0; i <= 5; i++) {
+    if (i < num) {
+      array.push(1);
+    } else {
+      array.push(0);
+    }
+  }
+  return array;
+}
+function converToCastString(casts) {
+  var castjoin = "";
+  for (var ids in casts) {
+    castjoin = castjoin + casts[ids].name + "/";
+  }
+  return castjoin.substring(0, castjoin.length - 2)
+}
+function converToCastInfo(casts) {
+  var castArray = [];
+  for (var ids in casts) {
+    var cast = {
+      img: casts[ids].avatars ? casts[ids].avatars.large : '',
+      name: casts[ids].name
+    };
+    castArray.push(cast);
+  }
+  return castArray
+}
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  starscanvas2Array: starscanvas2Array,
+  converToCastString: converToCastString,
+  converToCastInfo: converToCastInfo
 }
